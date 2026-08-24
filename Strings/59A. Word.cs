211afaf6bfdd324f6c386362f6b4670dc041c58d
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2026-08-24 20:59:49 +0300

    Add files via upload

---
+++ Strings/59A. Word.cs
@@ -0,0 +1,32 @@
+﻿
+// Author : Mohamed Abdelbaky Tony 
+// Topic : Strings 
+// Source : Codeforces 
+// Rate : 800
+// Problem Link : https://codeforces.com/contest/59/problem/A
+
+using System.Diagnostics.Metrics;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int Capital = 0, Small = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] <= 90)
+                    Capital++;
+                else
+                    Small++;
+            }
+            if (Small >= Capital)
+                Console.WriteLine(input.ToLower());
+            else
+                Console.WriteLine(input.ToUpper());
+        }
+    }
+}
