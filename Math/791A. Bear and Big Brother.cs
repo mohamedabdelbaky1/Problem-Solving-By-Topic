6f164a6b22b8d21725e39a6348b63a65785282a2
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2026-08-24 17:09:37 +0300

    Add files via upload

---
+++ Math/791A. Bear and Big Brother.cs
@@ -0,0 +1,23 @@
+﻿
+// Author : Mohamed Abdelbaky Tony 
+// Topic : Math 
+// Source : Codeforces 
+// Rate : 800
+// Problem Link : https://codeforces.com/contest/791/problem/A
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Input = Console.ReadLine().Split();
+            int a = int.Parse(Input[0]);
+            int b = int.Parse(Input[1]);
+
+            Console.WriteLine((int)(Math.Log10((double)b/a) / Math.Log10(3.0/2.0))+1);
+
+
+        }
+    }
+}
