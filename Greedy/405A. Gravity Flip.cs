f37df287c0fc31a5003d7c629eec407c777d5f57
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2026-08-24 20:34:09 +0300

    Add files via upload

---
+++ Greedy/405A. Gravity Flip.cs
@@ -0,0 +1,26 @@
+﻿
+// Author : Mohamed Abdelbaky Tony 
+// Topic : Implementation 
+// Source : Codeforces 
+// Rate : 900
+// Problem Link : https://codeforces.com/contest/405/problem/A
+
+using System.Diagnostics.Metrics;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split() ,int.Parse);
+            Array.Sort(arr);
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
