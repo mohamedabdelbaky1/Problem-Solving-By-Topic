3d1e758f83870b1a7a16a2cc571ae678f53debd1
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2026-08-24 17:44:34 +0300

    Add files via upload

---
+++ Strings/734A. Anton and Danik.cs
@@ -0,0 +1,36 @@
+﻿
+// Author : Mohamed Abdelbaky Tony 
+// Topic : Strings 
+// Source : Codeforces 
+// Rate : 800
+// Problem Link : https://codeforces.com/contest/734/problem/A
+using System.Diagnostics.Metrics;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int t = int.Parse(Console.ReadLine());
+            string x = Console.ReadLine();
+            int A = 0, D = 0;
+            foreach(char i in x)
+            {
+                if (i == 'A')
+                    A++;
+                else
+                    D++;
+            }
+            if (A > D)
+                Console.WriteLine("Anton");
+            else if (A < D)
+                Console.WriteLine("Danik");
+            else
+                Console.WriteLine("Friendship");
+
+        }
+    }
+}
