26828b0195419b569ac9cf4406d31f4b4a9e1ac5
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2026-08-24 22:36:42 +0300

    Add files via upload

---
+++ Implementation/344A. Magnets.cs
@@ -0,0 +1,37 @@
+﻿
+// Author : Mohamed Abdelbaky Tony 
+// Topic : Implementation 
+// Source : Codeforces 
+// Rate : 800
+// Problem Link : https://codeforces.com/contest/344/problem/A
+
+using System.Diagnostics.Metrics;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] arr = new string[n];
+
+            for (int i=0; i<n;i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+            int counter = 1;
+            for(int i=1;i<n;i++)
+            {
+                if (arr[i] != arr[i-1])
+                {
+                       counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
