05b5b4871e0f21b5f19c4825455766c48efe9701
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2026-08-24 20:53:05 +0300

    Add files via upload

---
+++ Strings/236A. Boy or Girl.cs
@@ -0,0 +1,25 @@
+﻿
+// Author : Mohamed Abdelbaky Tony 
+// Topic : Strings 
+// Source : Codeforces 
+// Rate : 800
+// Problem Link : https://codeforces.com/contest/236/problem/A
+
+using System.Diagnostics.Metrics;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int DistinctCount = new HashSet<char>(input).Count();
+            if (DistinctCount % 2 == 0)
+                Console.WriteLine("CHAT WITH HER!");
+            else
+                Console.WriteLine("IGNORE HIM!");
+        }
+    }
+}
