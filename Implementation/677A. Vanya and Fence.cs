c031ad212ee71a4a582539deb44dcaf41701797a
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2026-08-24 17:28:32 +0300

    Add files via upload

---
+++ Implementation/677A. Vanya and Fence.cs
@@ -0,0 +1,35 @@
+﻿
+// Author : Mohamed Abdelbaky Tony 
+// Topic : Implementation 
+// Source : Codeforces 
+// Rate : 800
+// Problem Link : https://codeforces.com/contest/677/problem/A
+
+using System.Diagnostics.Metrics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nh = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n = nh[0];
+            int h = nh[1];
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int Counter = 0;
+            for (int i=0;i<n;i++)
+            {
+                if (arr[i] > h)
+                    Counter += 2;
+                else
+                    Counter++;
+            }
+            Console.WriteLine(Counter);
+
+
+        }
+    }
+}
