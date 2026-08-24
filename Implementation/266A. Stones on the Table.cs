6bce4a765e136c58e3303c04838372cb3b636abd
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2026-08-24 23:06:31 +0300

    Add files via upload

---
+++ Implementation/266A. Stones on the Table.cs
@@ -0,0 +1,28 @@
+﻿
+// Author : Mohamed Abdelbaky Tony 
+// Topic : Implementation 
+// Source : Codeforces 
+// Rate : 800
+// Problem Link : https://codeforces.com/contest/266/problem/A
+
+using System.Diagnostics.Metrics;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int Counter = 0;
+            string x = Console.ReadLine();
+            for(int i=1;i<n;i++)
+            {
+                if (x[i] == x[i - 1])
+                    Counter++;
+            }
+            Console.WriteLine(Counter);
+        }
+    }
+}
