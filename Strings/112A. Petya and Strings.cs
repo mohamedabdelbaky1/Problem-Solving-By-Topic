92fb8ceb6779dc0d68e4de213a3e956cffe5218a
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2026-08-24 20:43:16 +0300

    Add files via upload

---
+++ Strings/112A. Petya and Strings.cs
@@ -0,0 +1,27 @@
+﻿
+// Author : Mohamed Abdelbaky Tony 
+// Topic : Strings 
+// Source : Codeforces 
+// Rate : 800
+// Problem Link : https://codeforces.com/contest/112/problem/A
+
+using System.Diagnostics.Metrics;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+
+            x = x.ToLower();
+            y = y.ToLower();
+
+            int res = string.Compare(x, y);
+            Console.WriteLine(res);
+        }
+    }
+}
