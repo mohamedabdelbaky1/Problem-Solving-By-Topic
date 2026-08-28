523d5b9e4234b18f3d656b3fd441bd40dfea954d
mohamedabdelbaky1 <96800735+mohamedabdelbaky1@users.noreply.github.com>
2026-08-28 20:19:13 +0300

    Update and rename Binary Search/670D2. Magic Powder - 2.cpp to Binary Search/Codeforces/CF-1500-670D2. Magic Powder - 2.cpp

--- Binary Search/Codeforces/CF-1500-670D2. Magic Powder - 2.cpp
+++ Binary Search/Codeforces/CF-1500-670D2. Magic Powder - 2.cpp
@@ -1,4 +1,7 @@
-// Author : Mohamed Abd EL Baky
+// Author : Mohamed Abdelbaky Tony 
+// Topic : Binary Search 
+// Source : Codeforces 
+// Rate : 1500
 // Problem Link : https://codeforces.com/problemset/problem/670/D2
 
 #include<bits/stdc++.h>
